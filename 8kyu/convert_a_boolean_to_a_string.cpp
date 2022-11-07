04271704b9a80ac0792b8377426681b8e4132198
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2022-11-07 19:30:14 +0100

    Create convert_a_boolean_to_a_string.cpp

---
+++ 8kyu/convert_a_boolean_to_a_string.cpp
@@ -0,0 +1,9 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+//Note: Only valid inputs will be given.
+
+#include <string>
+
+std::string boolean_to_string(bool b){
+  return b ? "true" : "false";
+}
