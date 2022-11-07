dea970eac52c7fba7166cf6dae157544cc808260
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2022-11-07 19:41:53 +0100

    Create messi_goals_function.cpp

---
+++ 8kyu/messi_goals_function.cpp
@@ -0,0 +1,19 @@
+//Messi goals function
+
+//Messi is a soccer player with goals in three leagues:
+
+//    LaLiga
+//    Copa del Rey
+//    Champions
+
+//Complete the function to return his total number of goals in all three leagues.
+
+//Note: the input will always be valid.
+
+//For example:
+
+//5, 10, 2  -->  17
+
+int goals (int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
