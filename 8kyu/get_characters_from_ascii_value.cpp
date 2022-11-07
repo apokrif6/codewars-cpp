8654e45fb38c42eea80ff500234d4c25be60e9f1
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2022-11-07 19:28:50 +0100

    Create get_characters_from_ascii_value.cpp

---
+++ 8kyu/get_characters_from_ascii_value.cpp
@@ -0,0 +1,14 @@
+//Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+//Example:
+
+//get_char(65)
+
+//should return:
+
+//'A'
+
+char get_char(int i) {
+  char c = i;
+  return c;
+}
