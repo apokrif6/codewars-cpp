92d32a2a035e4097a0b3580d11ce83c728439bf4
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2022-11-07 19:47:54 +0100

    Create reverse_or_rotate.cpp

---
+++ 6kyu/reverse_or_rotate.cpp
@@ -0,0 +1,55 @@
+//The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+
+//If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+//If
+
+//    sz is <= 0 or if str is empty return ""
+//    sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+
+//Examples:
+
+//revrot("123456987654", 6) --> "234561876549"
+//revrot("123456987653", 6) --> "234561356789"
+//revrot("66443875", 4) --> "44668753"
+//revrot("66443875", 8) --> "64438756"
+//revrot("664438769", 8) --> "67834466"
+//revrot("123456779", 8) --> "23456771"
+//revrot("", 8) --> ""
+//revrot("123456779", 0) --> "" 
+//revrot("563000655734469485", 4) --> "0365065073456944"
+
+//Example of a string rotated to the left by one position:
+//s = "123456" gives "234561".
+
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+
+class RevRot
+{
+public:
+    static std::string revRot(const std::string &strng, unsigned int sz)
+    {
+      if (sz == 0) { return ""; }
+      std::string res = strng.substr(0, strng.size() / sz * sz);
+  
+      for (int i = 0; i < res.size(); i += sz)
+      {
+        auto begin = std::next(res.begin(), i);
+        auto end   = std::next(res.begin(), i + sz);
+        int sum = std::accumulate(begin, end, 0, RevRot::acc);
+        if (sum % 2) { std::rotate(begin, begin + 1, end); }
+        else { std::reverse(begin, end); }
+      }
+  
+      return res;
+    }
+    
+private:
+    static int acc(int total, char &charNum)
+    {
+        return total + pow( static_cast<int>(charNum - 48), 3 );
+    }
+};
