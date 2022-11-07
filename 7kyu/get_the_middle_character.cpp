bc3480473f8c1053217bff13411d946c5f94f4f2
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2022-11-07 19:50:05 +0100

    Create get_the_middle_character.cpp

---
+++ 7kyu/get_the_middle_character.cpp
@@ -0,0 +1,30 @@
+//You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+//#Examples:
+
+//Kata.getMiddle("test") should return "es"
+
+//Kata.getMiddle("testing") should return "t"
+
+//Kata.getMiddle("middle") should return "dd"
+
+//Kata.getMiddle("A") should return "A"
+
+//#Input
+
+//A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+//#Output
+
+//The middle character(s) of the word represented as a string.
+
+std::string get_middle(std::string input) 
+{
+    int length = input.size();
+    int middle = length / 2;
+  
+    if (length % 2 == 0)
+      return input.substr((middle) - 1, 2);
+  
+    return input.substr(middle, 1);
+}
