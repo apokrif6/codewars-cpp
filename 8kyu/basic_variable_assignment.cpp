abdb078fbdca84a799bedb976ef31e517ea597dd
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2023-03-27 20:55:04 +0200

    Create basic_variable_assignment.cpp

---
+++ 8kyu/basic_variable_assignment.cpp
@@ -0,0 +1,7 @@
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+#include <string>
+
+std::string a = "code";
+std::string b = "wa.rs";
+std::string name = a + b;
