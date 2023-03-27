fdfa557fb6376df0f0cf9c4a2df276896daf5b4e
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2023-03-27 20:33:11 +0200

    Create convert_a_boolean_to_a_strings_yes_or_no.cpp

---
+++ 8kyu/convert_a_boolean_to_a_strings_yes_or_no.cpp
@@ -0,0 +1,8 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+#include <string>
+
+std::string bool_to_word(bool value)
+{
+  return value ? "Yes" : "No";
+}
