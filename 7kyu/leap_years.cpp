556e1f6844a901ba30e335d8f14c01f0a0c7c10c
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2023-03-27 21:16:32 +0200

    Create leap_years.cpp

---
+++ 7kyu/leap_years.cpp
@@ -0,0 +1,16 @@
+//In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+//    years divisible by 4 are leap years
+//    but years divisible by 100 are not leap years
+//    but years divisible by 400 are leap years
+
+//Additional Notes:
+
+//    Only valid years (positive integers) will be tested, so you don't have to validate them
+
+//Examples can be found in the test fixture.
+
+bool IsLeapYear(int year) 
+{
+  return !((year % 4) || (!(year % 100) && year % 400));
+}
