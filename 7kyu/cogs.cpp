be76b874448a785a015ec8b13a13bea1bdc24fbc
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2023-08-19 23:50:46 +0200

    Create cogs.cpp

---
+++ 7kyu/cogs.cpp
@@ -0,0 +1,20 @@
+//You are given a list of cogs in a gear train
+
+//Each element represents the number of teeth of that cog
+
+//e.g. [100, 75] means
+
+//    1st cog has 100 teeth
+//    2nd cog has 75 teeth
+
+//If the first cog rotates clockwise at 1 RPM what is the RPM of the final cog?
+
+//(use negative numbers for anti-clockwise rotation)
+//Notes
+
+//    no two cogs share the same shaft
+
+double cog_rpm(const std::vector<int> &cogs)
+{
+  return double((cogs.size() % 2) ? 1 : -1) * cogs[0] / cogs[cogs.size() - 1];
+}
